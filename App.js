9c0cbea8772e0141484f207844865ac076d18858
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2020-12-02 16:07:03 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('initText');
+
+  useEffect(() => {
+
+    const getHelloWorld = async() =>{
+      let response = await fetch('http://192.168.0.111:8080');
+      const data = await response.json();
+      setText(data.text);
+    }
+
+    getHelloWorld();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>{text}</Text>
     </View>
   );
 }
