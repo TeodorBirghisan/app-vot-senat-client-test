524c8e0c374a06125eff0d70c5682ae7254915ce
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-01-08 17:20:46 +0200

    Bottom Navigator for Main CreateSession and SendEmail

--- App.js
+++ App.js
@@ -3,23 +3,26 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import Login from './screen/Login';
 import Signup from './screen/Signup';
 import MainScreen from './screen/MainScreen';
 import SessionScreen from './screen/SessionScreen';
 import HeaderContainer from './components/HeaderComponent/Header';
+import SendEmailScreen from './screen/SendEmailScreen';
 import CreateSessionScreen from './screen/CreateSessionScreen';
+import BottomNavigator from './components/Navigator/BottomNavigator';
 
 const Stack = createStackNavigator();
+const BottomTabs = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='CreateSession'>
-        <Stack.Screen name='CreateSession' component={CreateSessionScreen} />
+      <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name='Login' component={Login} />
-        <Stack.Screen name='MainScreen' component={MainScreen} />
+        <Stack.Screen name='BottomNavigator' component={BottomNavigator} />
         <Stack.Screen name='Session' component={SessionScreen} />
       </Stack.Navigator>
     </NavigationContainer>
