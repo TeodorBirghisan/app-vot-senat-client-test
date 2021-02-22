00720dcd5f852a26f3e4c6a01808f15a2335a96f
TeodorBirghisan <48990501+TeodorBirghisan@users.noreply.github.com>
2021-02-22 18:26:08 +0200

    Change of Host address

--- endpoints/Endpoints.js
+++ endpoints/Endpoints.js
@@ -1,4 +1,4 @@
-const HOST = 'http://192.168.1.5:8080';
+const HOST = 'http://192.168.1.2:8080';
 
 const apiLoginAdmin = `${HOST}/user/login-admin`;
 const apiLoginSuperAdmin = `${HOST}/user/login-super-admin`;
